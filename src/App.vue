1d1a518a25811d4253ab00803bcb5c64f3d94631
cacaomath <mahiro.1192@gmail.com>
2023-04-22 12:25:59 +0900

    chore: 画像の更新

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,14 @@
 <template>
   <v-app id="inspire">
-    <v-navigation-drawer model-value class="pt-4" color="grey-lighten-3" rail>
-      <v-avatar
-        v-for="n in 6"
-        :key="n"
-        :color="`grey-${n === 1 ? 'darken' : 'lighten'}-1`"
-        :size="n === 1 ? 36 : 20"
-        class="d-block text-center mx-auto mb-9"
-      ></v-avatar>
+    <v-navigation-drawer v-model="drawer">
+      <!--  -->
     </v-navigation-drawer>
+
+    <v-app-bar>
+      <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
+
+      <v-toolbar-title>cacaomath</v-toolbar-title>
+    </v-app-bar>
 
     <v-main>
       <BioItem></BioItem>
@@ -20,5 +20,6 @@
 import BioItem from './components/BioItem.vue';
 export default {
   components: { BioItem },
+  data: () => ({ drawer: false }),
 };
 </script>
