c487ab11c830c21d55206a87a9b7e5a196fcc5e2
cacaomath <mahiro.1192@gmail.com>
2023-04-24 23:59:58 +0900

    feat: カードアイテムにコンポーネント間で値を受け渡すようにした

---
+++ src/components/CardItems.vue
@@ -0,0 +1,14 @@
+<template>
+  <v-card height="200">
+    <v-card-title>{{ props.title }}</v-card-title>
+    <v-card-item>{{ props.content }}</v-card-item>
+  </v-card>
+</template>
+
+<script setup lang="ts">
+import { defineComponent } from 'vue';
+defineComponent({
+  name: 'CardItems',
+});
+const props = defineProps(['title', 'content']);
+</script>
--- src/views/WorksPage.vue
+++ src/views/WorksPage.vue
@@ -1,9 +1,79 @@
-<template>Works</template>
+<template>
+  <v-container>
+    a
+    <v-row>
+      <v-col
+        v-for="(item, key) in programmingCardItems"
+        :key="key"
+        cols="4"
+        align-self="center"
+      >
+        <CardItems :title="item.title" :content="item.content" />
+      </v-col>
+    </v-row>
+  </v-container>
+  <v-container>
+    a
+    <v-row>
+      <v-col
+        v-for="(item, key) in journalCardItems"
+        :key="key"
+        cols="4"
+        align-self="center"
+      >
+        <CardItems :title="item.title" :content="item.content" />
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
 
-<script lang="ts">
-import { defineComponent } from 'vue';
+<script setup lang="ts">
+import { Ref, defineComponent, ref } from 'vue';
+import CardItems from '../components/CardItems.vue';
 
-export default defineComponent({
+interface CardItem {
+  title: string;
+  content: string;
+}
+const journalCardItems: Ref<Array<CardItem>> = ref([
+  {
+    title: 'a',
+    content: 'none',
+  },
+  {
+    title: 'b',
+    content: 'none',
+  },
+  {
+    title: 'a',
+    content: 'none',
+  },
+  {
+    title: 'b',
+    content: 'none',
+  },
+]);
+
+const programmingCardItems: Ref<Array<CardItem>> = ref([
+  {
+    title: 'a',
+    content: 'none',
+  },
+  {
+    title: 'b',
+    content: 'none',
+  },
+  {
+    title: 'a',
+    content: 'none',
+  },
+  {
+    title: 'b',
+    content: 'none',
+  },
+]);
+defineComponent({
   name: 'WorksPage',
+  components: { CardItems },
 });
 </script>
