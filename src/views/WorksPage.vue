3b0630307a090ee1e0989c534cefc509474318f0
cacaomath <mahiro.1192@gmail.com>
2023-04-27 23:48:25 +0900

    feat: 研究について情報追加

--- src/views/WorksPage.vue
+++ src/views/WorksPage.vue
@@ -1,7 +1,11 @@
 <template>
-  <CardItems :item-list="journalCardItems" />
+  <v-container>
+    <h2>Research</h2>
+    <CardItems :item-list="journalCardItems" />
 
-  <CardItems :item-list="programmingCardItems" />
+    <h2>Programming</h2>
+    <CardItems :item-list="programmingCardItems" />
+  </v-container>
 </template>
 
 <script setup lang="ts">
@@ -10,28 +14,28 @@ import CardItems, { CardItem } from '../components/CardItems.vue';
 
 const journalCardItems: Array<CardItem> = [
   {
-    title: 'a',
-    subtitle: 'string',
-    content: 'none',
-    link: 'https://cacaomath.com',
+    title:
+      'Feature Analysis to Estimate Sleep Time Based on Simple Measurement of Biological Information after Awakening',
+    subtitle:
+      'Mahiro Imabeppu, Ren Katsurada, Tatsuhito Hasegawa; IJBBB; Jul. 2020',
+    content:
+      'Keyword: Wearable device, Sleep time estimation, Support vector machine, Electrooculogram.',
+    link: 'http://www.ijbbb.org/show-85-939-1.html',
   },
   {
-    title: 'b',
-    subtitle: 'string',
-    content: 'none',
-    link: 'https://cacaomath.com',
+    title:
+      'Effects of Different Activities on Learning Efficiency of m-Learning Users',
+    subtitle:
+      'Mahiro Imabeppu, 2021 IEEE TALE Tatsuhito Hasegawa; December. 2021',
+    content:
+      'Keywords: m-Learning, Learning Efficiency, Confidence Rating, Activity Recognition',
+    link: 'https://ieeexplore.ieee.org/document/9678604',
   },
   {
-    title: 'a',
-    subtitle: 'string',
-    content: 'none',
-    link: 'https://cacaomath.com',
-  },
-  {
-    title: 'b',
-    subtitle: 'string',
-    content: 'none',
-    link: 'https://cacaomath.com',
+    title: 'm-Learning時の行動や学習方法が学習効果や安全性に与える影響',
+    subtitle: '今別府 万大, 長谷川 達人; 情報処理学会論文誌; 2022/12/15',
+    content: 'Keyword: m-Learning, 歩きスマホ, 学習効果',
+    link: 'https://cir.nii.ac.jp/crid/1390575960085275392',
   },
 ];
 
